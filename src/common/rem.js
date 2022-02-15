84c35629677ab48ee85a0469911ef18671142563
pop-guo <906792895@qq.com>
2022-02-15 14:21:35 +0800

    add mobile

--- src/common/rem.js
+++ src/common/rem.js
@@ -1,10 +1,11 @@
 (function () {
     function setRem() {
-        if (document.documentElement.clientWidth >= 0) {
-            var deviceWidth = parseInt(document.documentElement.clientWidth / 192) + 'px';
+        if (window.navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i)) {
+            let deviceWidth = parseInt(document.documentElement.clientWidth / 38) + 'px';
             document.documentElement.style.cssText = "font-size:" + deviceWidth + "!important";
-        }else{
-            document.documentElement.style.cssText = "font-size:" + 16 + "!important";
+        } else {
+            let deviceWidth = parseInt(document.documentElement.clientWidth / 192) + 'px';
+            document.documentElement.style.cssText = "font-size:" + deviceWidth + "!important";
         }
     }
     window.onresize = setRem;
