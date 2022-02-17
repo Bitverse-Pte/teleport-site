4bea1ddd6ae3e539aa067e710aa52f3c134d4718
pop-guo <906792895@qq.com>
2022-02-17 11:50:31 +0800

    fix: path change

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,5 @@
 import React from 'react';
 const Home = React.lazy(() => import("views/home"));
-const Teleport = React.lazy(() => import("views/teleport"));
-const Mobile = React.lazy(() => import("views/mobile"));
 
 const routes = [
     {
@@ -9,16 +7,16 @@ const routes = [
         exact: true,
         component: Home
     },
-    {
-        path: '/teleport',
-        exact: true,
-        component: Teleport
-    },
-    {
-        path: '/Mobile',
-        exact: true,
-        component: Mobile
-    }
+    // {
+    //     path: '/teleport',
+    //     exact: true,
+    //     component: Teleport
+    // },
+    // {
+    //     path: '/Mobile',
+    //     exact: true,
+    //     component: Mobile
+    // }
 ]
 export default routes;
 
