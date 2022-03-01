35d2f3de4b7e7de8202b7e188767d8ffad3422a8
pop <906792895@qq.com>
2022-03-01 13:48:22 +0800

    fix css

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,6 +13,7 @@ export default function Home() {
 	}
 
 	useEffect(() => {
+		require('common/rem.js')
 		if (
 			window.navigator.userAgent.match(
 				/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i,
